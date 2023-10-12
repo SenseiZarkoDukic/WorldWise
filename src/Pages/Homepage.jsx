da299ba0d3e2aad3ab1560c45f837a445f039065
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-12 18:42:56 +0200

    Put NavLink instead of Link in the PageNav component

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h1>WorldWise</h1>
-      <Link to="/Pricing">Pricing</Link>
     </div>
   );
 }
