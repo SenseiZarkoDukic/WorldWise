f302a8088e279aecfebf7008864fe85f5464127c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-12 17:23:14 +0200

    Import BrowserRouter, Routes, and Route from react-router-dom and Product component and give route to that compoenent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./Pages/Product";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      {" "}
+      <Routes>
+        <Route path="Product" element={<Product />} />
+      </Routes>{" "}
+    </BrowserRouter>
+  );
 }
 
 export default App;
