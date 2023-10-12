42b3828c67cf10862915d85fe1cb7c054db9ac41
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-12 17:41:52 +0200

    Create new component PageNotFound and add routes to all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Homepage from "./Pages/Homepage";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       {" "}
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="Product" element={<Product />} />
+        <Route path="Pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>{" "}
     </BrowserRouter>
   );
