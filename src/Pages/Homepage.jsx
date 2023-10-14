e75eefb7124bd495d5a345f33242549816ae1b44
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-14 09:14:09 +0200

    Import to the main.jsx global styling with index.css from the starter files

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
 
       <Link to="App">Go to the app</Link>
