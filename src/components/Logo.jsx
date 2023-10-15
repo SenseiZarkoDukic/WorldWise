ef9d0d709f050790b0c32cecd84a33604d4aba64
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-16 00:10:10 +0200

    Add starter files to component folder

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,7 @@
 import styles from "./Logo.module.css";
-import { Link } from "react-router-dom";
 
 function Logo() {
-  return (
-    <Link to="/">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
-    </Link>
-  );
+  return <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />;
 }
 
 export default Logo;
