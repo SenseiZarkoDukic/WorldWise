a1d78d59ca27c776f179855e1caa04384696eb80
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-17 15:51:09 +0200

    Add Map to AppLayout

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
+      <Map />
     </div>
   );
 }
