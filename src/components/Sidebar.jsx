eeddbc28ac355f10520010de85e4d12b103b6f1f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-17 15:08:34 +0200

    Create Footer component with its module css file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,16 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+    </div>
+  );
 }
 
 export default Sidebar;
