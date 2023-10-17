d276cd857a4fbc21e871a65e273e005b445db1ff
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-17 15:11:06 +0200

    Add Footer component to the Sidebar component and style Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
+import Footer from "./Footer";
 
 function Sidebar() {
   return (
-    <div>
+    <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
       <p>List of cities</p>
+      <Footer />
     </div>
   );
 }
