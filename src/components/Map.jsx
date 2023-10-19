2f875a592bc7e2f9a75ed639ecb56adcb243a6ea
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-19 18:06:30 +0200

    use Navigate to set to the routed 'form' element

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
@@ -9,11 +11,14 @@ function Map() {
   console.log(lat, lng);
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
       </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change position
+      </button>
     </div>
   );
 }
