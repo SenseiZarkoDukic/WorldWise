6d0d5c14b26439b2d9ebbc204627579a486ad0b9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-20 16:45:42 +0200

    Give unique key value to CountryItem as country

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -18,7 +18,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} />
+        <CountryItem country={country} key={country} />
       ))}
     </ul>
   );
