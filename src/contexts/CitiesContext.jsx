980fbe542b0affa9eb8f571718ee572f85b44f90
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-30 15:58:58 +0100

    setCities, adding to the current citties a new city to fetch and render it right away

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -51,7 +51,7 @@ function CitiesProvider({ children }) {
       });
 
       const data = await res.json();
-      console.log(data);
+      setCities((cities) => [...cities, data]);
     } catch {
       alert("There was an error loading data...");
     } finally {
