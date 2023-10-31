1d44beff3d23bd1e4bb8aaed9277c8deb4087077
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-31 16:22:03 +0100

    use Reducer to add state and action to login and logout functions in the context

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,9 +1,44 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
 
 function AuthProvider({ children }) {
-  return <AuthContext.Provider value="">{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {}
+  function logout() {}
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
@@ -12,3 +47,5 @@ function useAuth() {
     throw new Error("useAuth must be used within a AuthProvider");
   }
 }
+
+export { AuthProvider, useAuth };
