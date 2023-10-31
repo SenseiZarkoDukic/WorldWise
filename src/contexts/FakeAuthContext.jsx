5f24068c2357af8224c83bfc637191a0ecfd395f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-31 16:04:48 +0100

    Boilerplate code for context of Authentication

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,3 +1,14 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  return <AuthContext.Provider value="">{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+}
