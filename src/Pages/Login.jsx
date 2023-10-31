6afa912c6eede85047ff2ef8ad529a1a23b651e9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-31 17:06:08 +0100

    Inside an effect, check whether isAuthenticated === true, if so, programatically navigate to /app

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
 import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-  const { login } = useAuth();
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/app");
+    }
+  });
   return (
     <main className={styles.login}>
       <PageNav />
