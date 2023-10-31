d4d379c3c3c4b0f0b6a1b44ae4045bdf2cdba50a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-31 16:55:01 +0100

    In the Login.jsx page, call login() from context

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login } = useAuth();
 
   return (
     <main className={styles.login}>
