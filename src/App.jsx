707cf4eab6bae0827e6acb3a60775b6c6ee90cd6
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-01 13:16:30 +0100

    Wrap ProtectedRoute component around AppLayout component inside App.jsx, use condition in return of ProtectedRoute children if isAuthenticated else null

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Form from "./components/Form";
 import { Navigate } from "react-router-dom";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./Pages/ProtectedRoute";
 
 function App() {
   return (
@@ -25,7 +26,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
 
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
--- src/Pages/ProtectedRoute.jsx
+++ src/Pages/ProtectedRoute.jsx
@@ -3,16 +3,16 @@ import { useAuth } from "../contexts/FakeAuthContext";
 import { useNavigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, user } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isAuthenticated) {
       navigate("/");
     }
-  }, [isAuthenticated, navigate]);
+  }, [isAuthenticated, navigate, user]);
 
-  return children;
+  return isAuthenticated ? children : null;
 }
 
 export default ProtectedRoute;
