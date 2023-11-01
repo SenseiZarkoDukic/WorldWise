0c9e752a2ae6a6d90d953d004f4b9397fd5754ad
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-01 12:39:21 +0100

    Create ProtectedRoute.jsx file in pages folder, useEffect to redirect if not authenticated to the homepage

---
+++ src/Pages/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
+  return children;
+}
+
+export default ProtectedRoute;
