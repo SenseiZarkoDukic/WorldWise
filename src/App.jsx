0a350973e879606020082ddf185882b5f219553b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-03 18:22:41 +0100

    Run npm run build, to see how big is bundle before lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./Pages/ProtectedRoute";
+import { lazy } from "react";
+
 import Product from "./Pages/Product";
 import Pricing from "./Pages/Pricing";
 import Homepage from "./Pages/Homepage";
 import PageNotFound from "./Pages/PageNotFound";
 import AppLayout from "./Pages/AppLayout";
 import Login from "./Pages/Login";
+
+// const Homepage = lazy(() => import("./Pages/Homepage"));
+
+// dist/index.html                   0.48 kB │ gzip:   0.32 kB
+// dist/assets/index-84cc8aaf.css   31.32 kB │ gzip:   5.22 kB
+// dist/assets/index-b45fb1a4.js   527.74 kB │ gzip: 149.63 kB
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { Navigate } from "react-router-dom";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./Pages/ProtectedRoute";
 
 function App() {
   return (
